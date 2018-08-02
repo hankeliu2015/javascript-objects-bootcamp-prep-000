970efa94dc20f9183ef4eedbd9352611017d5c0c
Hank Liu <hanke.liu@gmail.com>
2018-08-02 03:31:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,11 +6,14 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song
+  // playlist[artist] = song
+  
+  playlist = {
+    artist: song
+  };
   return playlist
     
 }
-
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist.artist;
